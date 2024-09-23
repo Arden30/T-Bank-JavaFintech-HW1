e38f513b76d396c4d65e79fdd009e69ce757349c
Arsenev Denis <denis.arseniev1995@gmail.com>
2024-09-23 11:43:20 +0300

    feat: init Spring project (KudaGo crud service)

---
+++ KudaGo/src/main/java/arden/java/kudago/KudaGoApplication.java
@@ -0,0 +1,13 @@
+package arden.java.kudago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KudaGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KudaGoApplication.class, args);
+	}
+
+}
---
+++ KudaGo/src/test/java/arden/java/kudago/KudaGoApplicationTests.java
@@ -0,0 +1,13 @@
+package arden.java.kudago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KudaGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
