907f4d64036f98d731713c628e082d44dd15b793
Arsenev Denis <denis.arseniev1995@gmail.com>
2024-09-30 19:15:35 +0300

    fix: getter for creation object exception

--- KudaGo/src/main/java/arden/java/kudago/exception/CreationObjectException.java
+++ KudaGo/src/main/java/arden/java/kudago/exception/CreationObjectException.java
@@ -1,8 +1,10 @@
 package arden.java.kudago.exception;
 
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 
+@Getter
 public class CreationObjectException extends GeneralException {
     private final String description = "Can't create a new object";
     private final HttpStatusCode httpStatusCode = HttpStatus.NOT_FOUND;
