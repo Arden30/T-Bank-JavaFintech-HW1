12f2737d53ed452dc471c278db30744e4386dfa0
Arsenev Denis <denis.arseniev1995@gmail.com>
2024-10-02 01:04:58 +0300

    fix: added build.gradle for starter module

---
+++ KudaGo/starter/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("java")
+    id("org.springframework.boot") version "3.3.4"
+    id("io.spring.dependency-management") version "1.1.6"
+}
+
+val aspectVersion: String by project
+group = "configuration.annotation.logtimexec"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.aspectj:aspectjweaver:${aspectVersion}")
+    implementation("org.aspectj:aspectjrt:${aspectVersion}")
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
